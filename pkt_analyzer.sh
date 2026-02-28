49bf9abe67a6a6f3556bee73ed1973b336b15e90
Lwimix <martilwinx@gmail.com>
2026-02-28 22:50:41 +0300

    Unique DNS addresses

--- pkt_analyzer.sh
+++ pkt_analyzer.sh
@@ -11,7 +11,7 @@ while [[ true ]]; do
 	echo "2: NBNS: NetBios Name Service"
 	printf "\n"
 	read option
-	if [[ $option -eq 0 ]]; then
+	if [[ $option -eq 0 ]]; then #Gen Info
 		host_ips=$(tshark -r $1 -Y "http.request.method == GET" -T fields -e ip.src -e eth.src | tr , '\n' | sort -u | awk '{print $1,$2}' | tr ' ' '\t')
 		startofcapture=$(tshark -r $1 -o gui.column.format:"Time,%Yut" | sort -n | head -1)
 		endofcapture=$(tshark -r $1 -o gui.column.format:"Time,%Yut" | sort -nr | head -1)
@@ -26,14 +26,14 @@ while [[ true ]]; do
 		printf "End of capture: $endofcapture\n"
 		printf "Total capture time: $timeinminutes min $timeinseconds sec\n"
 		printf "#############################\n"
-	elif [[ $option -eq 1 ]]; then
-		dns_ips=$(tshark -r $1 -Y "dns.flags.response==1" -T fields -e dns.a -e dns.resp.name | tr , '\n' | awk '$1!=""' | awk '$2!="" {print $1, $2}' | tr ' ' '\t')
+	elif [[ $option -eq 1 ]]; then #DNS
+		dns_ips=$(tshark -r $1 -Y "dns.flags.response==1" -T fields -e dns.a -e dns.resp.name | tr , '\n' | awk '$1!=""' | awk '$2!="" {print $1, $2}' | tr ' ' '\t' |  awk '!seen[$0]++')
 		printf "\n"
 		printf "#############################\n"
 		printf "***********DNS A Records*****************\n"
 		printf "Ipv4 address\tHostname\n"
 		printf "${dns_ips[@]}\n"
-	elif [[ $option -eq 2  ]]; then
+	elif [[ $option -eq 2  ]]; then #NBNS
 		nbns_ips=$(tshark -r $1 -Y 'nbns.flags.opcode in {5,8}' -T fields -e nbns.name -e nbns.addr | tr , '\n' | awk '$2!="" {print$1,$3}' | sort -u | tr ' ' '\t')
 		printf "\n"
 		printf "#############################\n"
